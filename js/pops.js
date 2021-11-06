14031e6c2f0b5502add0d3fd978b9f139bb21703
Cristiano10 <ccristiandarioromero@gmail.com>
2021-11-06 13:59:31 -0300

    new version

---
+++ js/pops.js
@@ -0,0 +1,73 @@
+function popup1() {
+  popup('Mi proceso de desarrollo', '<ul class="ulmodal"><li>Definir funcionalidades y caracteristicas de la app:'+
+    '<ul class="ulmodal"><li>Para qué se usará?</li>'+
+     '<li>Cuáles son las necesidades funcionales del usuario?</li>'+
+     '<li>Cuál es el perfil de los usuarios?</li>'+
+     '<li>Para qué y cómo se usará</li>'+
+      '</ul></li>'+
+            '<li>Analizo soluciones para que respondan a las necesidades del usuario de la informacion obtenida en fase anterior.</li>'+
+            '<li>Programación de las especificacion de diseño de la interfaz, incluir funcionalidades para responder a las interacciones del usuario.</li>'+
+            '<li>El sitio web se pone a prueba navegando entre sus diferentes apartados y menús en diferentes exploradores y dispositivos.</li>'+
+            '<li>La Interfaz queda preparada para facilitar su mantenimiento y realizar futuras actualizaciones.</li>'+
+      '</ul>');
+}
+
+
+function popup3() {
+        popup('<p>Programador, recibido en técnico en informática en el colegio Juan XXIII de Ramos Mejía donde aprendí:</p>', 
+          '<ul class="ulmodal"><li>Algoritmos y estructuras de datos</li>'+
+            '<li>lenguajes de programación como java y c++</li>'+
+            '<li>hacer consultas de bases de datos usando sql </li>'+
+            '</ul>'+
+            '<p class="pmodal">Años después me vuelvo bueno en:</p>'+
+          '<ul class="ulmodal"><li> Html CSS y JS.</li>'+
+            '<li>Comencé con jquery básico, DOM y otras cosas en las etapas iniciales.</li>'+
+            '<li>Utilizo Media Queries para diseño responsivo.</li>'+
+            '<li>Me llevé bien con estilos y diseños predefinidos.</li>'+
+            '<li> Mis plataformas empiezan a interactuar con clientes y usuarios.</li>'+
+            '</ul>'+
+            '<p class="pmodal">He estado trabajando en sitios web monolíticos y SPA durante cinco años</p>'+
+          '<ul class="ulmodal"><li>Programo y desarrollo softwares, webapps y sitios web.</li>'+
+            '<li>Mis apps consumen datos de manera local o remota.</li>'+
+            '<li>Diseño servidores con rutas programadas que responden a las peticiones de usuarios.</li>'+
+            '<li>Administro base de datos para filtrar su contenido.</li>'+
+            '<li>Protego mis rutas para que el usuario se autentique y pueda empezar a pedir cosas al servidor.</li>'+
+            '</ul>'
+            ,
+
+
+          { width: '400px', height: 'auto' })
+    }
+    function popup2() {
+  popup('Asi programo una webapp',
+      '<ul class="ulmodal sepciall">'+
+            '<li>Creo y diseño la base de datos.</li>'+
+            '<li>Creo servidor http o consumo los servicios de uno ya creado para esperar las peticiones del cliente permitiendo al frontend intercambiar datos con la base de datos.</li>'+
+            '<li>Programo las rutas donde el cliente enviara sus peticiones.</li>'+
+
+            '<li>Maqueto la estructura básica de la web a nivel imagenes, textos osea a nivel de contenido</li>'+
+            '<li>Con CSS le otorgo estilos a cada etiqueta del sitio para que se va bonita para el usuario</li>'+
+            '<li>JS empieza a darle interacciuon a los objetos de la pagina y que respondan a ciertos eventos del usuario.</li>'+
+            
+            '<li>Defino los flujos de interaccion finales de usuario para ir realizaando las tareas del sitio como por ejemplo el inicio de sesion</li>'+
+            '<li>Se definen tipografías, colores, y jarquias de elementos de la web.</li>'+
+            
+            '<li>Mi app esta lista para actualizar base de datos y enviar FE al cliente</li>'+
+            '</ul>');
+}
+
+function popup4() {
+        popup('<p class="p-modal-angular">Proyecto Angular<br><span class="spanmodal">Recuperación de Posts FB</span></p>', 
+          '<ul class="specialul">'+
+            '<li class="before1"><img src="../img/modal1/s11.png"></li>'+
+            '<li class="before2"><img src="../img/modal1/s2.png"></li>'+
+            '<li class="before3"><img src="../img/modal1/s3.png"></li>'+
+            '<li class="before4"><img src="../img/modal1/s4.png"></li>'+
+            '<li class="before5"><img src="../img/modal1/s5.png"></li>'+
+            '<li class="before6"><img src="../img/modal1/s6.png"></li>'+
+            '<li class="before7"><img src="../img/modal1/s9.png"></li>'+
+            '<li class="before8"><img src="../img/modal1/sf2.png"></li>'+
+            '</ul>'
+            ,
+          { width: '400px', height: 'auto' })
+    } 
